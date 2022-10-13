2f4caf572b634ebfb460e419fbbced9a6bc18cab
GertieMeneer <gertjan.vat@icloud.com>
2022-10-13 10:05:56 +0200

    ayo i changed this

--- Weekly Assignment/src/Utilities.java
+++ Weekly Assignment/src/Utilities.java
@@ -8,23 +8,23 @@ public class Utilities {
     }
 
     public String toString() {
-        String outsidetemp = ("Buitentemperatuur: " + rounder(measurement.getOutsideTemperature()) + " graden Celcius");
-        String insidetemp = ("Binnentemperatuur: " + rounder(measurement.getInsideTemperature()) + " graden Celcius");
-        String airpressure = ("Luchtdruk: " + rounder(measurement.getAirPressure()) + " hPa");
-        String insidehum = ("Luchtvochtigheid binnen: " + rounder(measurement.getInsideHum()) + " %");
-        String outsidehum = ("Luchtvochtigheid buiten: " + rounder(measurement.getOutsideHum()) + " %");
-        String windspeed = ("Windsnelheid: " + rounder(measurement.getWindSpeed()) + " KM/h");
-        String avgwindspeed = ("Gemiddelde windsnelheid: " + rounder(measurement.getAvgWindSpeed()) + " KM/h");
-        String windir = ("Windrichting: " + measurement.getWindDirection());
-        String rainrate = ("Hoeveelheid neerslag: " + rounder(measurement.getRainRate()));
+        String outsidetemp = ("Outsidetemperature: " + rounder(measurement.getOutsideTemperature()) + " graden Celcius");
+        String insidetemp = ("Insidetemperature: " + rounder(measurement.getInsideTemperature()) + " graden Celcius");
+        String airpressure = ("Airpressure: " + rounder(measurement.getAirPressure()) + " hPa");
+        String insidehum = ("Humidity Inside: " + rounder(measurement.getInsideHum()) + " %");
+        String outsidehum = ("Humidity Outside: " + rounder(measurement.getOutsideHum()) + " %");
+        String windspeed = ("Windspeed: " + rounder(measurement.getWindSpeed()) + " KM/h");
+        String avgwindspeed = ("Average Windspeed: " + rounder(measurement.getAvgWindSpeed()) + " KM/h");
+        String windir = ("Winddirection: " + measurement.getWindDirection());
+        String rainrate = ("Rainrate: " + rounder(measurement.getRainRate()));
         String uvlevel = ("UV index: " + measurement.getUVLevel());
-        String solarrad = ("Zonintensiteit: " + rounder(measurement.getSolarRad()) + " W/m2");
-        String xmitbatt = ("Batterijniveau station: " + measurement.getXmitBatt() + " %");
-        String consbatt = ("Batterijniveau console: " + measurement.getBattLevel() + " %");
+        String solarrad = ("Solar Radiation: " + rounder(measurement.getSolarRad()) + " W/m2");
+        String xmitbatt = ("Battery Station: " + measurement.getXmitBatt() + " %");
+        String consbatt = ("Battery Console: " + measurement.getBattLevel() + " %");
         String foricon = ("Icon: " + measurement.getForeIcon());
-        String sunrise = ("Zonsopgang: " + measurement.getSunrise());
-        String sunset = ("Zonsondergang: " + measurement.getSunset());
-        String dewpoint = ("Dauwpunt: " + rounder(measurement.getDewPoint()) + " graden Celcius");
+        String sunrise = ("Sunrise: " + measurement.getSunrise());
+        String sunset = ("Sunset: " + measurement.getSunset());
+        String dewpoint = ("Dewpoint: " + rounder(measurement.getDewPoint()) + " graden Celcius");
         String windchill = ("Wind Chill: " + rounder(measurement.getWindChill()) + " graden Celcius");
         String heatindex = ("Heat Index: " + rounder(measurement.getHeatIndex()) + " graden Celcius");
 
@@ -45,3 +45,4 @@ public class Utilities {
         return number;
     }
 }
+
